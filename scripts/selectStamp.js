21c16cf8486b9f721a1fc4c3832ed8f54af61297
test <test>
2025-06-29 06:58:26 +0900

    test

--- scripts/selectStamp.js
+++ scripts/selectStamp.js
@@ -1,28 +1,15 @@
 
-const { Jimp } = require('jimp');
-
 module.exports = (robot) => {
-  const onfile = (res, file) => {
-    res.download(file, async (path) => {
-      let ext = file.name.slice(-4);  // オリジナルの拡張子を取得
-      let newFileName = Math.random().toString(32).substring(2) + ext;   // ランダムなファイル名を生成
-      try {
-        console.log('path: ' + path);
-        let image = await Jimp.read(path);
-        image.blur(7);
-        await image.write('images/' + newFileName);
-        res.send({
-          path: 'images/' + newFileName
-        });
-      }
-      catch (err) {
-        res.send('Error: ' + err);
-        return;
-      }
-    });
-  }
+    const onfile = (res, file) => {
+        res.send([
+            'File received.',
+            `name: ${file.name}`,
+            `type: ${file.content_type}`,
+            `size: ${file.content_size}bytes`,
+        ].join('\n'));
+    };
 
-  robot.respond('file', (res) => {  // ファイルがアップロードされたときの処理
-    onfile(res, res.json);
-  });
-}
+    robot.respond('file', (res) => {
+        onfile(res, res.json);
+    });
+};
